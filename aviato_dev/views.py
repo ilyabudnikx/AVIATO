a243dd7e9c4994d892284fa75569363d4e36384e
ilyabudnikx <ilyabudnikxs@mail.ru>
2022-03-09 21:15:11 +0300

    re-up views avito_dev

--- aviato_dev/views.py
+++ aviato_dev/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def MAINPAGE(request):
